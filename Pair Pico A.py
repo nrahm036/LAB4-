61e8bc84abc2c255c62d75464e2d5955637d2c16
nrahm036 <146502857+nrahm036@users.noreply.github.com>
2023-11-06 17:11:11 -0500

    Update Pair Pico A.py

--- Pair Pico A.py
+++ Pair Pico A.py
@@ -31,28 +31,3 @@ while True:
     
     recieved_message = ""
     #(pico b) recieves pwm and processes
-    
-    if uart.any():
-        recieved_message = uart.read().decode() .strip()
-        
-        #checks if recieved_message has desired pwm value
-        if "The PWM value is" in  recieved_message:
-            pass
-        elif "The anolog value is" in recieved_message:
-            pass 
-        else:
-            #something weird happened?
-            print ("This was not what I was expecting!")
-
-        
-            #expected_pwm_value = float(recieved_message.split(":")[1].strip()[:-1])
-
-            # Split the string using ":" and convert to float 
-            calculated_pwm_value = (pwm_reciever.duty_u16() / 65535) * 100              
-            # 65535 is the max 16 bit value, this gives percent of duty cycle
-            
-            difference = expected_pwm_value - calculated_pwm_value
-            print ("the measured pwm value was:{:.2F}%" .format(calculated_pwm_value))
-            print ("the difference in pwm value was:{:.2F}%" .format(difference))
-            uart.write("Received and Processed: {:.2f}%".format(calculated_pwm_value))  
-            #sends response back to pico a, includes measured pwm value
