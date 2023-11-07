d0f5c6ca3f30786d12661cb692a6efcf6d12644b
nrahm036 <146502857+nrahm036@users.noreply.github.com>
2023-11-07 14:41:28 -0500

    Update Pair Pico A.py

--- Pair Pico A.py
+++ Pair Pico A.py
@@ -12,8 +12,8 @@ from machine import UART
 uart = UART(1, 9600, tx=Pin(8), rx=Pin(9))
 uart.init(9600, bits=8, parity=None, stop=1)
 
-#esablishes pwm output on pico a 
-p8 = machine.Pin(8)
+#establishes pwm output on pico a 
+p8 = machine.Pin(0)
 pwm8 = machine.PWM(p8)
 print(type(pwm8))
 # Set PWM frequency to be sent to Pico B, on pin 8
@@ -22,12 +22,15 @@ pwm8.freq(1000)
 pwm8.duty_u16(32768)                                                                      
 
 #initialize reciever on pico b 8=tx, reciever pin
-pwm_reciever = machine.PWM(Pin(8))
+# pwm_reciever = machine.PWM(Pin(8))
 
 #(pico a) sends pwm signal, waits 10 seconds before sending signal again
 while True:
     uart.write("Duty cycle is =" + str(pwm8.duty_u16()))
-    time.sleep(10)
+    time.sleep(5)
     
     recieved_message = ""
     #(pico b) recieves pwm and processes
+    
+    
+
